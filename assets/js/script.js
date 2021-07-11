6c271fe07405d4a6ef0259d9985b42638c111e30
Jodi Jackson <jjacksonjayhawkfan@gmail.com>
2021-07-11 14:42:46 -0500

    Add dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,17 @@ Style the new task item.
 Add the text.
 
 Append this element to the task list.*/
-var buttonEl = document.querySelector("#save-task"); 
-var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() { 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+var createTaskHandler = function () {
 
-buttonEl.addEventListener("click", createTaskHandler);
+    event.preventDefault();
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
